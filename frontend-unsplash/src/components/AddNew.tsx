ee7b7701f5f4b8ce12bd81565249167eaa1f25f8
Psahu1296 <pushpendra.sahu0202@gmail.com>
2023-05-21 10:53:16 +0530

    search and delete functionality added

--- frontend-unsplash/src/components/AddNew.tsx
+++ frontend-unsplash/src/components/AddNew.tsx
@@ -1,48 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { IPhotoData } from "./HomePage/page";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
-
 
 interface IAddNew {
   photoFormhandler: (args: boolean) => void;
   setPhotoList: (args: IPhotoData[]) => void;
-  photoList: IPhotoData[]
+  photoList: IPhotoData[];
   setIsLoading: (args: boolean) => void;
 }
-const AddNew = ({photoList, photoFormhandler, setPhotoList, setIsLoading}: IAddNew) => {
-
-  const [form, setForm] = useState<IPhotoData>({id: '', label: '', imageURL: ''});
+const AddNew = ({
+  photoList,
+  photoFormhandler,
+  setPhotoList,
+  setIsLoading,
+}: IAddNew) => {
+  const [form, setForm] = useState<IPhotoData>({
+    label: "",
+    imageURL: "",
+    date: null,
+  });
 
   const addPhtoHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === 'Label') {
-      setForm({...form, label: e.target.value});
-      return
-    } 
-    setForm({...form, imageURL: e.target.value});
+    if (e.target.name === "Label") {
+      setForm({ ...form, label: e.target.value });
+      return;
     }
+    setForm({ ...form, imageURL: e.target.value });
+  };
 
-  const onSubmitHandler = async(e: React.FormEvent) => {
-    e.preventDefault()
-    setIsLoading(true)
-    try {const id = uuidv4().toString();
-    const updateId  = {...form, id}
-    const data = [updateId,...photoList]
-    console.log("response: " , { body: data})
-    setPhotoList([updateId,...photoList])
-    const response = await axios.post("http://localhost:5000/image", {...updateId})
-    setIsLoading(false)
-    photoFormhandler(false)}
-    catch (error) {
+  const onSubmitHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const date = new Date()
+      const updateId = { ...form, date };
+      const data = [updateId, ...photoList];
+      setPhotoList([updateId, ...photoList]);
+      const response = await axios.post("http://localhost:5000/image", {
+        ...updateId,
+      });
+    } catch (error) {
       console.log("mongodb error: ", error);
-      setIsLoading(false)
-      alert("something went wrong")
+      alert("something went wrong");
+    } finally {
+      photoFormhandler(false);
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
     <div className="fixed z-[1000] bg-black-100/25 h-[100vh] w-[100vw] flex justify-center">
-      <form className="absolute z-10 w-[620px] rounded-[12px] bg-white flex flex-col top-[20%] p-5" onSubmit={onSubmitHandler}>
+      <form
+        className="absolute z-10 w-[620px] rounded-[12px] bg-white flex flex-col top-[20%] p-5"
+        onSubmit={onSubmitHandler}
+      >
         <label className="text-primary text-[24px] font-[500] mb-[20px]">
           Add a new photo
         </label>
@@ -66,10 +78,13 @@ const AddNew = ({photoList, photoFormhandler, setPhotoList, setIsLoading}: IAddN
           onChange={addPhtoHandler}
         />
         <div className="w-full flex">
-          <button className="min-w-auto h-[55px] text-tertiary rounded-[12px] bg-none active:scale-[0.95] ml-auto" onClick={(e: any) => {
-            e.preventDefault()
-            photoFormhandler(false)
-          }}>
+          <button
+            className="min-w-auto h-[55px] text-tertiary rounded-[12px] bg-none active:scale-[0.95] ml-auto"
+            onClick={(e: any) => {
+              e.preventDefault();
+              photoFormhandler(false);
+            }}
+          >
             Cancel
           </button>
           <button className="min-w-[150px] h-[55px] text-white rounded-[12px] bg-green active:scale-[0.95] ml-3">
